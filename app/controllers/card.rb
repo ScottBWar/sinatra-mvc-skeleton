6e22488f242d02efd2d66f2fa87ac24be7081ec2
Ari Perez <aris.a.perez@gmail.com>
2015-05-02 19:06:58 -0400

    More func sync changes

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -5,10 +5,14 @@ get '/card/:card_id' do
     round = Round.find_or_create_by(live: true, deck_id: params[:current_deck_id], user_id: current_user.id)
     Guess.where(id: round.guess_ids).each do |guess|
       guessed_cards << guess.card_id
+      p guessed_cards
     end
     card_array = deck.card_ids - guessed_cards
+    if !card_array
+      erb :"/deck/all?round_over=true"
+    end
     flash_card = Card.find(card_array.sample)
-    erb :"/card/card", locals:{flash_card: flash_card}
+    erb :"/card/show", locals:{flash_card: flash_card}
   else
     redirect "/"
   end
@@ -19,5 +23,5 @@ post '/card/:card_id' do
   round = Round.find_by(live: true, user_id: current_user.id)
   guess = Guess.create(round: round, card_id: params[:card_id], result: (card.answer.downcase == params[:answer].downcase))
   round.guesses << guess
-  redirect "/cards/:card_id"
+  redirect "/card/params[:card_id]"
 end
