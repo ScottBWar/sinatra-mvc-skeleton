f70f0e3f157debb267ef6158b2d7b5a7542604e4
Ari Perez <aris.a.perez@gmail.com>
2015-05-02 18:33:42 -0400

    Merges Nik and Ari's changes

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -1,11 +1,23 @@
-get "/deck/:deckid/card/new" do
- erb :"card/new" ,locals: {deck_id: params[:deckid]}
+get '/card/:card_id' do
+  if current_user
+    guessed_cards = []
+    deck = Deck.find(params[:current_deck_id])
+    round = Round.find_or_create_by(live: true, deck_id: params[:current_deck_id], user_id: current_user.id)
+    Guess.where(id: round.guess_ids).each do |guess|
+      guessed_cards << guess.card_id
+    end
+    card_array = deck.card_ids - guessed_cards
+    flash_card = Card.find(card_array.sample)
+    erb :"/card/card", locals:{flash_card: flash_card}
+  else
+    redirect "/"
+  end
 end
 
-post "/deck/:deckid/card" do
-  new_card = Card.new(question: params[:question], answer: params[:answer], deck_id: params[:deckid])
-  new_card.save
-   redirect "/deck/#{params[:deckid]}/card/new"
-
-
+post '/card/:card_id' do
+  card = Card.find(params[:card_id])
+  round = Round.find_by(live: true, user_id: current_user.id)
+  guess = Guess.create(round: round, card_id: params[:card_id], result: (card.answer.downcase == params[:answer].downcase))
+  round.guesses << guess
+  redirect "/cards/:card_id"
 end
